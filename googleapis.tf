ca45772666be25ccc0a47ccf01fe53f16992ae8d
mriswithe <1725647+mriswithe@users.noreply.github.com>
2025-03-06 14:18:38 -0600

    enabling APIs

---
+++ googleapis.tf
@@ -0,0 +1,9 @@
+resource "google_project_service" "project" {
+  for_each = toset(["iam", "run", "cloudresourcemanager"])
+  service  = join(".", [each.value, "googleapis.com"])
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
+  disable_on_destroy = false
+}
